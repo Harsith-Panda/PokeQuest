1b9881bff189cf07afaf27b4fca9703831a0afd5
Harsith Priyan Sivanandham <harsithpriyan@gmail.com>
2025-11-20 15:43:47 +0530

    Vercel Final

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,23 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
+  // try {
+  //   await mongoose.connect(process.env.MONGO_URL);
+  //   console.log("Connected to MongoDB");
+  // } catch (error) {
+  //   console.error("Error connecting to MongoDB:", error);
+  //   process.exit(1);
+  // }
+  //
+  if (mongoose.connection.readyState) {
+    console.log("MongoDB is already connected!");
+    return;
+  }
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
+    console.log("MongoDB connected successfully!");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
